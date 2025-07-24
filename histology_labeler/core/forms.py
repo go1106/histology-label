2f5fee7e02149dc4a4f35552f6a2091163490047
go1106 <luckyjia2025@gmail.com>
2025-07-23 19:34:59 -0500

    Delete histology_labeler directory

--- histology_labeler/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import SlideImage
-
-class SlideImageForm(forms.ModelForm):
-    class Meta:
-        model = SlideImage
-        fields = ['title', 'image_file']
